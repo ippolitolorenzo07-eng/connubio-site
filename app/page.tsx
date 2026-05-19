5ff439a248a038a0ce892d2c68fb7bee176fecc7
ippolitolorenzo07-eng <ippolitolorenzo07@gmail.com>
2026-05-19 15:30:26 +0200

    added experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero"
+import Experience from "@/components/Experience"
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Experience />
     </main>
   )
 }
